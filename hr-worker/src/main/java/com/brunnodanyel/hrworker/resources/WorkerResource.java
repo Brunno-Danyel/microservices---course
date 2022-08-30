383733c5043364528647a8b096fbe07bfbccdbd0
Brunno Danyel <brunnodanyel25rbr@hotmail.com>
2022-08-30 17:26:39 -0300

    hr-worker as a config server client

--- hr-worker/src/main/java/com/brunnodanyel/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/brunnodanyel/hrworker/resources/WorkerResource.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,18 +16,27 @@ import org.springframework.web.bind.annotation.RestController;
 import com.brunnodanyel.hrworker.entities.Worker;
 import com.brunnodanyel.hrworker.repositories.WorkerRepository;
 
-import lombok.AllArgsConstructor;
 
-@AllArgsConstructor
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
 
 	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
+	@Value("${test.config}")
+	private String testConfig;
+
+	@Autowired
 	private Environment env;
 
+	@Autowired
 	private WorkerRepository repository;
+
+	@GetMapping(value = "/configs")
+	public ResponseEntity<Void> getConfigs() {
+		logger.info("CONFIG = " + testConfig);
+		return ResponseEntity.noContent().build();
+	}
 
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll() {
