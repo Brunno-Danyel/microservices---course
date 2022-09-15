52d6372c1c79d476d232fa6d51a61c8e566bf50b
Brunno Danyel <brunnodanyel25rbr@hotmail.com>
2022-09-15 16:06:48 -0300

    <Correção II>

--- hr-oauth/src/main/java/com/brunnodanyel/hroauth/config/AuthorizationServerConfig.java
+++ hr-oauth/src/main/java/com/brunnodanyel/hroauth/config/AuthorizationServerConfig.java
@@ -17,10 +17,10 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
-	@Value("($oauth.client.name}")
+	@Value("${oauth.client.name}")
 	private String clientName;
 	
-	@Value("($oauth.client.secret}")
+	@Value("${oauth.client.secret}")
 	private String clientSecret;
 	
 	@Autowired
