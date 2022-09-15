991839a5402a3c91e15ce8957cc292ba8eef623d
Brunno Danyel <brunnodanyel25rbr@hotmail.com>
2022-09-15 14:29:04 -0300

    Security configs to config server

--- hr-api-gateway-zuul/src/main/java/com/brunnodanyel/hrapigatewayzuul/config/AppConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/brunnodanyel/hrapigatewayzuul/config/AppConfig.java
@@ -1,10 +1,16 @@
 package com.brunnodanyel.hrapigatewayzuul.config;
 
+<<<<<<< Updated upstream
+=======
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+>>>>>>> Stashed changes
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+<<<<<<< Updated upstream
 @Configuration
 public class AppConfig {
 
@@ -12,6 +18,19 @@ public class AppConfig {
 	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
 		tokenConverter.setSigningKey("MY-SECRET-KEY");
+=======
+@RefreshScope
+@Configuration
+public class AppConfig {
+
+	@Value("${jwt.secret}")
+	private String jwtSecret;
+	
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+		tokenConverter.setSigningKey("jwtSecret");
+>>>>>>> Stashed changes
 		return tokenConverter;
 	}
 
