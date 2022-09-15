1d37fe1e8c849206a5b521d2dc0bb7dce0b9becc
Brunno Danyel <brunnodanyel25rbr@hotmail.com>
2022-09-15 16:04:08 -0300

    <Correções>

--- hr-api-gateway-zuul/src/main/java/com/brunnodanyel/hrapigatewayzuul/config/AppConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/brunnodanyel/hrapigatewayzuul/config/AppConfig.java
@@ -1,24 +1,13 @@
 package com.brunnodanyel.hrapigatewayzuul.config;
 
-<<<<<<< Updated upstream
-=======
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
->>>>>>> Stashed changes
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
-<<<<<<< Updated upstream
-@Configuration
-public class AppConfig {
 
-	@Bean
-	public JwtAccessTokenConverter accessTokenConverter() {
-		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-		tokenConverter.setSigningKey("MY-SECRET-KEY");
-=======
 @RefreshScope
 @Configuration
 public class AppConfig {
@@ -29,8 +18,7 @@ public class AppConfig {
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-		tokenConverter.setSigningKey("jwtSecret");
->>>>>>> Stashed changes
+		tokenConverter.setSigningKey(jwtSecret);
 		return tokenConverter;
 	}
 
--- hr-api-gateway-zuul/src/main/java/com/brunnodanyel/hrapigatewayzuul/config/ResourceServerConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/brunnodanyel/hrapigatewayzuul/config/ResourceServerConfig.java
@@ -20,11 +20,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	
 	private static final String[] OPERATOR = {"/hr-worker/**"};
 	
-<<<<<<< Updated upstream
-	private static final String[] ADMIN = {"/hr-payroll/**", "/hr-user/**"};
-=======
 	private static final String[] ADMIN = {"/hr-payroll/**", "/hr-user/**", "/actuator/**", "/hr-worker/actuator/**","/hr-oauth/actuator/**"};
->>>>>>> Stashed changes
+
 	
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
